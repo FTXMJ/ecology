c5b14d4494f35bab75708600d693f75bd12d1ded
FTXMJ <15044827567@163.com>
2019-12-17 18:49:59 +0800

    update code

--- models/force_table.go
+++ models/force_table.go
@@ -1,15 +1,15 @@
 package models
 
 type ForceTable struct {
-	Id    int    `orm:"column(id);pk;auto"`
+	Id    int    `orm:"column(id);pk;auto" json:"id"`
 	Level string `orm:column(level)`
 	//LevelInt               string  `orm:column(level_int)`          //等级数字
-	LowHold             int     `orm:column(low_hold)`              //低位
-	HighHold            int     `orm:column(high_hold)`             //高位
-	ReturnMultiple      float64 `orm:column(return_multiple)`       //杠杆
-	HoldReturnRate      float64 `orm:column(hold_return_rate)`      //本金自由算力
-	RecommendReturnRate float64 `orm:column(recommend_return_rate)` //直推算力
-	TeamReturnRate      float64 `orm:column(team_return_rate)`      //动态算力
+	LowHold             int     `orm:column(low_hold) json:"low_hold"`                           //低位
+	HighHold            int     `orm:column(high_hold) json:"high_hold"`                         //高位
+	ReturnMultiple      float64 `orm:column(return_multiple) json:"return_multiple"`             //杠杆
+	HoldReturnRate      float64 `orm:column(hold_return_rate) json:"hold_return_rate"`           //本金自由算力
+	RecommendReturnRate float64 `orm:column(recommend_return_rate) json:"recommend_return_rate"` //直推算力
+	TeamReturnRate      float64 `orm:column(team_return_rate) json:"team_return_rate"`           //动态算力
 }
 
 // 快速排序
