00667207626f160c6d13d034560717b7a569d5d7
FTXMJ <15044827567@163.com>
2020-01-04 18:29:01 +0800

    update code

--- models/account_handler.go
+++ models/account_handler.go
@@ -107,7 +107,7 @@ func SuperLevelSet(user_id string, ec_obj *Ecology_index_obj, tfor float64) {
 		}
 	}
 	blo := TxIdList{}
-	NewOrm().Raw("select * from tx_id_list where user_id=? and comment=? and create_time>=? and create_time<=? limit 1", user_id, "节点分红", time.Now().Format("2006-01-02")+" 00:00:00", time.Now().Format("2006-01-02")+" 59:59:59").QueryRow(&blo)
+	NewOrm().Raw("select * from tx_id_list where user_id=? and comment=? and create_time>=? and create_time<=? order by create_time desc limit 1", user_id, "节点分红", time.Now().Format("2006-01-02")+" 00:00:00", time.Now().Format("2006-01-02")+" 59:59:59").QueryRow(&blo)
 	if blo.Id < 1 {
 		blo.Expenditure = 0
 	}
