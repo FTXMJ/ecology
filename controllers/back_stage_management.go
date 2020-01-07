f0080a053a815f943f1a171daed0f6520fb6fada
FTXMJ <15044827567@163.com>
2020-01-07 15:31:33 +0800

    update code

--- controllers/back_stage_management.go
+++ controllers/back_stage_management.go
@@ -786,13 +786,13 @@ func (this *BackStageManagement) PeerABounsList() {
 	peer_history := []models.PeerHistory{}
 	switch start_time {
 	case "":
-		_, err := models.NewOrm().Raw("select * from peer_history where time>=? and time<=?", start_time, end_time).QueryRows(&peer_history)
+		_, err := models.NewOrm().Raw("select * from peer_history").QueryRows(&peer_history)
 		if err != nil {
 			data = common.NewResponse(models.PeerHistoryList{})
 			return
 		}
 	default:
-		_, err := models.NewOrm().Raw("select * from peer_history").QueryRows(&peer_history)
+		_, err := models.NewOrm().Raw("select * from peer_history where time>=? and time<=?", start_time, end_time).QueryRows(&peer_history)
 		if err != nil {
 			data = common.NewResponse(models.PeerHistoryList{})
 			return
