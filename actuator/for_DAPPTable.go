399964b0b010fb56235e8618c98d9f04fb3a62aa
FTXMJ <15044827567@163.com>
2020-01-14 19:13:22 +0800

    update code

---
+++ actuator/for_DAPPTable.go
@@ -0,0 +1,38 @@
+package actuator
+
+import (
+	"ecology/models"
+	"errors"
+	"github.com/astaxie/beego/orm"
+)
+
+func SelectDAPP(o orm.Ormer, dapp_name, dapp_id, dapp_type string, page *models.Page) (dapp_list []DAPPTable, err error) {
+	q := o.QueryTable("dapp_table")
+	if dapp_name != "" {
+		q.Filter("name", dapp_name)
+	}
+	if dapp_id != "" {
+		q.Filter("id", dapp_id)
+	}
+	if dapp_type != "" {
+		q.Filter("dapp_type", dapp_type)
+	}
+	q.All(&dapp_list)
+
+	start, end := InitPage(page, len(dapp_list))
+
+	if end > len(dapp_list) && start < len(dapp_list) {
+
+		return dapp_list[start:], nil
+
+	} else if start > len(dapp_list) {
+
+		return []DAPPTable{}, nil
+
+	} else if end < len(dapp_list) && start < len(dapp_list) {
+
+		return dapp_list[start:end], nil
+
+	}
+	return dapp_list, errors.New("err")
+}
